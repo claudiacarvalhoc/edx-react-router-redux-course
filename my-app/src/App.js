0f817cec577e841e81766696959246007ca22162
sky-claudiacrisostomo <claudia.crisostomo@sky.uk>
2019-09-23 14:43:28 +0100

    add links to the homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { render } from "react-dom";
+import { Link, NavLink } from 'react-router-dom'
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { About } from "./About";
@@ -8,16 +8,29 @@ import { Home } from "./Home";
 import { Users } from "./Users";
 
 const App = () => (
-  <div>
-    <BrowserRouter>
+  <BrowserRouter>
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users/abc">Users: abc</Link>
+        </li>
+        <li>
+          <Link to="/info">Info</Link>
+        </li>
+      </ul>
+      <hr />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/users/:id" component={Users} />
       <Route path="/info" component={Info} />
     </div>
   </BrowserRouter>
-  </div>
 );
 
 
