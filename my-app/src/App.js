ceb24f2c98c2d721299427830ee8162f8505e1fa
sky-claudiacrisostomo <claudia.crisostomo@sky.uk>
2019-09-23 14:45:24 +0100

    add navlinks to homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,50 @@
 import React from "react";
 // import { render } from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
 import { About } from "./About";
-import { Info } from "./Info";
+// import { Info } from "./Info";
 import { Home } from "./Home";
 import { Users } from "./Users";
 
+var style = {
+  color: "red"
+};
+
+// const activeEvent = (match, location) => {
+//   if (!match) {
+//     return false;
+//   }
+//   //do something
+//   console.log("active on : " + match.url);
+// };
+
 const App = () => (
-  <div>
-    <BrowserRouter>
+  <BrowserRouter>
     <div>
+      <ul>
+        <li>
+          <NavLink exact activeStyle={style} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={style} to="/about">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={style} to="/users/abc">
+            Users: abc
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/users/:id" component={Users} />
-      <Route path="/info" component={Info} />
     </div>
   </BrowserRouter>
-  </div>
 );
 
 
